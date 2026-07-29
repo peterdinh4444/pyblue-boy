2de8f8ac454502e7e5956a6d86480bceaf7817da
Peter <peterdd06@gmail.com>
2026-07-28 23:53:29 -0400

    Entities and Sprites files

--- code/main.py
+++ code/main.py
@@ -1,28 +1,49 @@
 from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join, dirname, abspath #for file handling OS
+
+from sprites import Sprite
+from entities import Player
+
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Pyboy-Blue')
 
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+
         self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
 
     def import_assets(self):
+        #MAP SETUP
         BASE_DIR = dirname(abspath(__file__))
         map_path = join(BASE_DIR, '..', 'data', 'maps', 'world.tmx')
         self.tmx_maps = {'world': load_pygame(map_path)}
         print(self.tmx_maps)
-        
+
+    def setup(self, tmx_map, player_start_pos):
+        for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            Sprite((x*TILE_SIZE,y*TILE_SIZE), surf, self.all_sprites)
+
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            print(obj)
+
     def run(self):
         while True:
+            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
 
-                pygame.display.update()
+
+            # game logic
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
 
 
 if __name__ == '__main__':
